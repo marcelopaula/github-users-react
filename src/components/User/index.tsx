0d6fc25c2fe4a69aa375bb91dd7954c8f1519d96
marcelopaula <marcelopaula.rj@gmail.com>
2023-12-26 17:11:41 -0300

    adjustment to user data layout;

--- src/components/User/index.tsx
+++ src/components/User/index.tsx
@@ -2,32 +2,42 @@
 import { RootState } from '@/Store';
 import { Grid, Avatar } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
+import PeopleAltOutlined from '@mui/icons-material/PeopleAltOutlined';
 import { useSelector } from 'react-redux';
+import * as S from './styles';
 
 const User = () => {
-    const user = useSelector((state:RootState) => state.user);
+	const user = useSelector((state:RootState) => state.user);
 
-    return (
-        <Grid container>
-            <Grid xs={4}>
-                <Avatar
-                    alt={user.name}
-                    src={user.avatar_url}
-                />
-            </Grid>
-            <Grid xs={8}>
-                <Typography>{user.name}</Typography>
-                <Typography>{user.email}</Typography>
-                <Typography>Bio: {user.bio}</Typography>
-            </Grid>
-
-
-            <Typography>Seguidores: {user.followers}</Typography>
-            <Typography>Seguindo: {user.following}</Typography>
-
-            <hr />
-        </Grid>
-    )
+	return (
+		<S.Wrapper>
+			<Grid container>
+				<Grid item xs={2}>
+						<Avatar
+								alt={user.name}
+								src={user.avatar_url}
+								style={{width: '100px', height: '100px'}}
+						/>
+				</Grid>
+				<Grid xs={10}>
+					<Grid container>
+						<Grid item>
+							<Typography variant='h6'>{user.name}</Typography>
+							<Typography variant='body2'>Bio: {user.bio}</Typography>
+						</Grid>
+					</Grid>
+					<Grid container spacing={2} alignItems='center'>
+						<Grid item>
+							<PeopleAltOutlined />
+						</Grid>
+						<Grid item>
+							<Typography variant='caption'>Seguidores: {user.followers} Seguindo: {user.following}</Typography>
+						</Grid>
+					</Grid>
+				</Grid>
+			</Grid>
+		</S.Wrapper>
+	)
 };
 
 export default User;
