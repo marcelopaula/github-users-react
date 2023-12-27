c0e1dd406715bd367cabff0833903c97c0d31552
marcelopaula <marcelopaula.rj@gmail.com>
2023-12-27 01:28:36 -0300

    Adjusting Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,12 @@ import User from '@/components/User';
 
 export default function Home() {
   return (
-    <>
-      <S.Home>
-        <Grid container justifyContent='center' alignItems='center'>
-          <Grid item md={6} lg={4}>
-            <Search />
-          </Grid>
+    <S.Home>
+      <Grid container justifyContent='center' alignItems='center'>
+        <Grid item md={6} lg={4}>
+          <Search />
         </Grid>
-      </S.Home>
-    </>
+      </Grid>
+    </S.Home>
   )
 }
