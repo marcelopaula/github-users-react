5fe8f0865f43bf5ff1f9bb06ecad18c62386f72c
marcelopaula <marcelopaula.rj@gmail.com>
2023-12-27 00:15:37 -0300

    Adjustment of repository page layout

--- src/app/repo/[name]/page.tsx
+++ src/app/repo/[name]/page.tsx
@@ -1,19 +1,22 @@
 'use client'
 
 import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation'
+import { useRouter } from 'next/navigation';
 import { useSelector } from 'react-redux';
-import { getRepository } from '@/services/Repositories';
 import { RootState } from '@/Store';
+import { getRepository } from '@/services/Repositories';
 import { IRepository } from '@/types/repository';
-import Typography from '@material-ui/core/Typography';
+import { Typography, Link, Grid } from '@material-ui/core';
 import StarIcon from '@mui/icons-material/Star';
 import LinkIcon from '@mui/icons-material/Link';
 import Stack from '@mui/material/Stack';
-import Link from '@material-ui/core/Link';
 import * as S from './styles';
+
+import Header from '@/components/Header';
+import BreadCrumbs from '@/components/BreadCrumbs';
  
 export default function Page({params}:{params: {name: string}}) {
+  const router = useRouter();
   const [data, setData] = useState<IRepository>();
   const user = useSelector((state:RootState) => state.user);
 
@@ -23,37 +26,42 @@ export default function Page({params}:{params: {name: string}}) {
   }
 
   useEffect(() => {
+    if (!user.login) {
+      router.push('/')
+    }
     if (params.name) loadRepository(params.name);
   }, [])
 
   return (
-    <main>
+    <>
+      <Header />
+      <BreadCrumbs repo={params.name} />
       {
         data && (
-          <>
-            <S.Section>
-              <Stack direction="row" spacing={1} alignItems="center">
-                <LinkIcon color='primary' style={{width: '20px', height: '20px'}} />
-                <Link variant='h5'>{data.name}</Link>
-                <StarIcon color='primary' style={{width: '20px', height: '20px'}} />
-                <Typography variant='body2'>
-                  {data.stargazers_count}
-                </Typography>
-              </Stack>
-            </S.Section>
+          <Grid container justifyContent='center'>
+            <Grid item md={8} lg={6}>
+              <Grid container justifyContent="center" direction='column' spacing={2}>
+                <Grid item xs>
+                  <Stack direction="row" spacing={1} alignItems="center">
+                    <LinkIcon color='primary' style={{width: '20px', height: '20px'}} />
+                    <Link variant='h5' href={data.html_url} target='_blank'>{data.name}</Link>
+                    <StarIcon color='primary' style={{width: '20px', height: '20px'}} />
+                    <Typography variant='body2'>
+                      {data.stargazers_count}
+                    </Typography>
+                  </Stack>
+                </Grid>
 
-            <S.Section>
-            </S.Section>
-
-            <S.Section>
-              <Typography variant='body1'>Descrição:</Typography>
-              <Typography variant='caption'>{data.description}</Typography>
-            </S.Section>
-          </>
+                <Grid item xs>
+                  <Typography variant='body1'>Descrição:</Typography>
+                  <Typography variant='caption'>{data.description}</Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
         )
       }
-
-    </main>
+    </>
 
     )
 }
